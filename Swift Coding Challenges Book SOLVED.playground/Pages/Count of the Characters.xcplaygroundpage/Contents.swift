37d6fa08f498c4e0e3290b20d201c0cc7fbc5736
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 11:01:27 +0530

    5th Solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Count of the Characters.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Count of the Characters.xcplaygroundpage/Contents.swift
@@ -2,6 +2,34 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+private func countSpecificCharacterInString(inputStr: String, specificStr: Character) -> Int {
+    var count = 0
+    
+    for char in inputStr {
+        if char == specificStr {
+            count += 1
+        }
+    }
+    
+    return count
+}
 
-//: [Next](@next)
+
+countSpecificCharacterInString(inputStr: "Mississippi", specificStr: "i")
+countSpecificCharacterInString(inputStr: "Hacking with Swift", specificStr: "i")
+
+
+// Using Reduce Func
+private func countSpecificCharacterInStringUsingReduceFunc(inputStr: String, specificStr: Character) -> Int {
+    return inputStr.reduce(0) {
+        $1 == specificStr ? $0 + 1 : $0
+    }
+}
+
+
+// Using Replacing Occurrences
+private func countSpecificCharacterInStringUsingReplacing(inputStr: String, specificStr: String) -> Int {
+    let modifiedStr = inputStr.replacingOccurrences(of: specificStr, with: "")
+    
+    return inputStr.count - modifiedStr.count
+}
