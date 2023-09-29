3a93e7a366905d06c7352a056080074f529b5592
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 18:28:59 +0530

    Binary Reverse Solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Binary Reverse.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Binary Reverse.xcplaygroundpage/Contents.swift
@@ -1,3 +1,13 @@
-//: [Previous](@previous)
 
 import Foundation
+
+
+private func reverseBinary(inputInt: UInt) -> UInt {
+    let binary = String(inputInt, radix: 2)
+    let paddingAmount = 8 - binary.count
+    let paddedBinary = String(repeating: "0", count: paddingAmount) + binary
+    let reversedBinary = String(paddedBinary.reversed())
+    return UInt(reversedBinary, radix: 2)!
+}
+
+reverseBinary(inputInt: 41)
