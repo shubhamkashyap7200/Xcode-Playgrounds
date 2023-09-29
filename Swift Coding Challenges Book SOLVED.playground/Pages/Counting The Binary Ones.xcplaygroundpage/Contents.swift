9e2ff5d7fd12dba53a9a84afbdcbe8fe806f0375
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 18:14:13 +0530

    Counting next higest and next lowest same binary 1 Question Solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Counting The Binary Ones.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Counting The Binary Ones.xcplaygroundpage/Contents.swift
@@ -2,6 +2,46 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+private func countTheBinaryOnes(inputInt: Int) -> (nextHightest: Int?, nextLowest: Int?) {
+    let targetBinary = String(inputInt, radix: 2)
+    
+    let targetOnes = targetBinary.filter {
+        (char: Character) -> Bool in
+        char == "1"
+    }.count
+    
+    var nextHighest: Int? = nil
+    var nextLowest: Int? = nil
+    
+    for i in inputInt + 1...Int.max {
+        let currentBinary = String(i, radix: 2)
+        let currentOnes = currentBinary.filter { (char: Character) -> Bool in char == "1" }.count
+        
+        if targetOnes == currentOnes {
+            nextHighest = i
+            break
+        }
+    }
+    
+    for i in (0..<inputInt).reversed() {
+        let currentBinary = String(i, radix: 2)
+        let currentOnes = currentBinary.filter { (char: Character) -> Bool in char == "1" }.count
 
-//: [Next](@next)
+        if targetOnes == currentOnes {
+            nextLowest = i
+            break
+        }
+
+    }
+    
+    
+    return (nextHighest,nextLowest)
+}
+
+countTheBinaryOnes(inputInt: 28)
+
+
+//
+//for i in (0..<10).reversed() {
+//    print(i)
+//}
