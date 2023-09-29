6990e7367b01ad5737d9d41a5d85a8f18986dc59
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 17:47:07 +0530

    is Prime Completed

--- Swift Coding Challenges Book SOLVED.playground/Pages/Number is Prime.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Number is Prime.xcplaygroundpage/Contents.swift
@@ -2,6 +2,36 @@
 
 import Foundation
 
-private func isNumberPrime(inputInt: Int) -> String {
-    guard inputInt <= 0 else { return "Invalid Number" }
+
+// Using Brute Force
+private func isNumberPrime(inputInt: Int) -> Bool {
+    guard inputInt >= 2 else { return false }
+    
+    for i in 2..<inputInt {
+        if inputInt % i == 0 {
+            return false
+        }
+    }
+    
+    return true
 }
+
+//isNumberPrime(inputInt: 16777259)
+
+// Using Optimised Path
+private func isNumberPrimeUsingOptimisedPath(inputInt: Int) -> Bool {
+    guard inputInt >= 2 else { return false }
+    guard inputInt != 2 else { return true }
+    
+    let max = Int(ceil(sqrt(Double(inputInt))))
+    
+    for i in 2...max {
+        if inputInt % i == 0 {
+            return false
+        }
+    }
+    
+    return true
+}
+
+isNumberPrimeUsingOptimisedPath(inputInt: 3)
