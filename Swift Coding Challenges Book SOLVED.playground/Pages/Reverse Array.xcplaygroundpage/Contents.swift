2aa32ad4e2d50a757b987d979764a39f2fd9ed24
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 12:31:24 +0530

    11th Question Solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Reverse Array.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Reverse Array.xcplaygroundpage/Contents.swift
@@ -15,5 +15,20 @@ private func reverseArray(inputStr: [Any]) -> [Any] {
     return reversedArray
 }
 
-
 reverseArray(inputStr: ["Apple", "1", "Sony", "Lenovo", "Asus"])
+
+
+private func reverseString(str: String) -> String {
+    var inputStr = Array(str)
+    var reversedArray = [Character]()
+    let intialIndex = 0
+    let finalIndex = inputStr.count - 1
+
+    for index in stride(from: finalIndex, through: intialIndex, by: -1) {
+        reversedArray.append(inputStr[index])
+    }
+    
+    return String(reversedArray)
+}
+
+reverseString(str: "Hello")
--- Swift Coding Challenges Book SOLVED.playground/Pages/Three Different Letters.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Three Different Letters.xcplaygroundpage/Contents.swift
@@ -2,6 +2,25 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+private func areThreeLetterDifferent(inputStr1: String, inputStr2: String) -> Bool {
+    guard inputStr1.count == inputStr2.count else { return false }
+    let firstArray = Array(inputStr1)
+    let secondArray = Array(inputStr2)
+    var countOfDifferentCharacters = 0
+    
+    for (index, char) in firstArray.enumerated() {
+        if secondArray[index] != char {
+            countOfDifferentCharacters += 1
+        }
+    }
+    
+    return (countOfDifferentCharacters <= 3 && countOfDifferentCharacters > 0) ? true : false
+    
+}
 
-//: [Next](@next)
+areThreeLetterDifferent(inputStr1: "Clamp", inputStr2: "Cramp")
+areThreeLetterDifferent(inputStr1: "Clamp", inputStr2: "Crams")
+areThreeLetterDifferent(inputStr1: "Clamp", inputStr2: "Grams")
+areThreeLetterDifferent(inputStr1: "Clamp", inputStr2: "Grans")
+areThreeLetterDifferent(inputStr1: "Clamp", inputStr2: "Clam")
+areThreeLetterDifferent(inputStr1: "clamp", inputStr2: "maple")
