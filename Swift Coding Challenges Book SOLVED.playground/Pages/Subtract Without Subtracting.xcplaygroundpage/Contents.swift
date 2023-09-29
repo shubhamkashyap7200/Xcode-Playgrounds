00e995e370b84bfb7e8a08b5e3c0e4f8b650d5aa
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-29 22:45:13 +0530

    Subtract Without Subtracting

---
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Subtract Without Subtracting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+private func customSubtraction(inputInt1: Int, inputInt2: Int) -> Int {
+    return inputInt1 + -1 * inputInt2
+}
+
+customSubtraction(inputInt1: 10, inputInt2: 5)
