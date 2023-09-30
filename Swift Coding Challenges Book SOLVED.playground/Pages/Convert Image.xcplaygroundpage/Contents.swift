0775de7f47f44e82c823edebed7b2d730a1320cf
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 13:47:19 +0530

    convert image from jph or pjpeg to png question solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Convert Image.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Convert Image.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
 //: [Previous](@previous)
 
 import Foundation
+import Cocoa
+import AppKit
 
-var greeting = "Hello, playground"
+// For iOS -- This code will only work id UIKit is imported
+//private func convertImagesForiOS(in directory: String) {
+//    let fm = FileManager.default
+//    let directoryURL = URL(fileURLWithPath: directory)
+//    guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return }
+//
+//    for file in files {
+//        guard file.pathExtension == "jpg" || file.pathExtension == " jpeg" else { continue }
+//        guard let image = UIImage(contentsOfFile: file.path) else { continue }
+//        guard let png = UIImagePNGRepresentation(image) else { continue }
+//
+//        let newFileName = file.deletingPathExtension().appendingPathExtension("png")
+//        _ = try? png.write(to: newFileName)
+//    }
+//}
 
-//: [Next](@next)
+
+// For macOS
+private func convertImagesForiOS(in directory: String) {
+    let fm = FileManager.default
+    let directoryURL = URL(fileURLWithPath: directory)
+    guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return }
+    
+    for file in files {
+        guard file.pathExtension == "jpg" || file.pathExtension == " jpeg" else { continue }
+        guard let image = NSImage(contentsOfFile: file.path) else { continue }
+        
+        guard let tiffData = image.tiffRepresentation else { continue }
+        guard let imagRep = NSBitmapImageRep(data: tiffData) else { continue }
+        guard let png = imagRep.representation(using: .png, properties: [:]) else { continue }
+        
+        
+        let newFileName = file.deletingPathExtension().appendingPathExtension("png")
+        _ = try? png.write(to: newFileName)
+    }
+}
+
+
