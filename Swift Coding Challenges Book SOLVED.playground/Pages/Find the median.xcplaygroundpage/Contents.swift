303a5769dab174338cb5127ee093dc84c4bc289d
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-02 15:27:42 +0530

    finding the median question solved

--- Swift Coding Challenges Book SOLVED.playground/Pages/Find the median.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges Book SOLVED.playground/Pages/Find the median.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+extension Collection where Iterator.Element == Int {
+    func findingTheMedian() -> Double? {
+        guard count != 0 else { return nil }
+        
+        let sorted = self.sorted()
+        let middlePoint = sorted.count / 2
+        
+        if sorted.count % 2 == 0 {
+            return Double(sorted[middlePoint] + sorted[middlePoint - 1]) / 2
+        } else {
+            return Double(sorted[middlePoint])
+        }
+    }
+}
 
-//: [Next](@next)
+
+[1, 2, 3, 4].findingTheMedian()
+[1, 3, 5, 7, 9].findingTheMedian()
+
